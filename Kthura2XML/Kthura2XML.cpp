fa073938fe792be6e85e02af85767f27db8476d2
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-14 17:12:17 +0100

    True test stuff comes later

---
+++ Kthura2XML/Kthura2XML.cpp
@@ -0,0 +1,11 @@
+#include <Kthura_Core.hpp>
+
+// Now don't expect TOO MUCH from this program
+// I didn't create it to make it all possible to export Kthura to XML
+// It's merely a debug program and the XML output is easy to read, so that can help me to see
+// if stuff works the way it should... Well, at least until I can actually do some
+// graphic representation of a Kthura Map. Sorry to disappoint ya!
+
+int main(int argc, char* arg) {
+
+}
