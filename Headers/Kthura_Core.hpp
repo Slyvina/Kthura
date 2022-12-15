48908b0e46a007144d7dd7e4a55bac6087f8cf01
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-15 15:51:58 +0100

    All in a day's work

--- Headers/Kthura_Core.hpp
+++ Headers/Kthura_Core.hpp
@@ -1,8 +1,29 @@
+// Lic:
+// Kthura/Headers/Kthura_Core.hpp
+// Slyvina - Kthura Core (header)
+// version: 22.12.15
+// Copyright (C) 2022 Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
 #pragma once
 #include <map>
+#include <string>
 #include <memory>
 #include <Slyvina.hpp>
 #include <SlyvString.hpp>
+#include <JCR6_Core.hpp>
 
 
 #define KthuraProp(type,prop) void prop(type); type prop()
@@ -27,22 +48,33 @@ namespace Slyvina {
 		class KthuraObject;
 		class KthuraLayer;
 
-		enum class KthuraKind { Unknown, TiledArea, StretchedArea, Rect, Zone, Obstacle, Picture, Actor };
+		enum class KthuraKind { Unknown, TiledArea, StretchedArea, Rect, Zone, Obstacle, Picture, Actor, Custom, Exit, Pivot };
 
 		class _Kthura {
 		private:
+			static uint64 _KthCount;
+			uint64 _KthID{ _KthCount++ };
 			std::map<std::string, KthuraLayer> _Layers;
 			std::vector<std::string> _LayersMap;
 			void _LayerRemap();
 			bool LayersRemapped{ false };
 		public:
+			static bool AllowUnknown; // If set to 'true' Kthura will load all entries found in a Kthura resource file (as long as in the same dir as Kthura's data) that it doesn't know in the memory and also save it with the map accordingly when saving. This setting is recommended for editors, but not for games using Kthura maps.
+			std::map<std::string, Units::Bank> Unknown{};
+			inline uint64 ID() { return _KthID; } // This number will be used by my graphics driver for disposing loaded textures it no longer needs.
 			StringMap MetaData = NewStringMap();
 			inline bool HasLayer(std::string Lay) { Units::Trans2Upper(Lay); return _Layers.count(Lay); }
 			KthuraLayer* Layer(std::string Lay);
 			inline std::vector<std::string>* Layers() { if (!LayersRemapped) _LayerRemap(); return &_LayersMap; }
 			KthuraLayer* NewLayer(std::string Lay, bool nopanic = false);
-			void KillLayer(std::string Lay, bool ignoreifnonexistent = false);
+			inline void KillLayer(std::string Lay, bool ignoreifnonexistent = false);
+
+			_Kthura(Slyvina::JCR6::JT_Dir Resource, std::string prefix = "");
+			inline _Kthura() { NewLayer("__BASE"); }
 		};
+
+		std::string KindName(KthuraKind k);
+		KthuraKind KindName(std::string s);
 
 		class KthuraObject {
 		private:
@@ -54,6 +86,7 @@ namespace Slyvina {
 			KthuraKind _Kind;
 			uint64 _ID{ 0 };
 		public:
+			std::string CustomKind{ "" }; // Only needs to be used when this is a custom kind
 			KthuraObject* DomNext{ nullptr }; // It would NOT be wise to alter this manually!
 			KthuraObject(KthuraObject* _after,KthuraLayer* _ouwe,KthuraKind _k,uint64 giveID);
 			inline KthuraObject* Next() { return _next; }
@@ -61,6 +94,9 @@ namespace Slyvina {
 			inline KthuraLayer* Parent() { return _parent; }
 			void __KillMe(); // NEVER use this directly! ALWAYS let the kill commands of the Kthura Layer do this.
 			inline int64 ID() { return _ID; }
+			inline KthuraKind Kind() { return _Kind; }
+			inline void IKind(KthuraKind K) { if (_Kind == KthuraKind::Unknown) _Kind = K; }
+			std::string SKind();
 
 			// Yeah, these are quick defintions done by macros.
 			// I do NOT need you to tell me that this is bad practise, because I already know and I don't care!
@@ -81,6 +117,17 @@ namespace Slyvina {
 			KthuraProp(uint32, dominance);
 			KthuraProp(int32, animspeed);
 			KthuraProp(uint32, animframe);
+			KthuraProp(int32, insertx);
+			KthuraProp(int32, inserty);
+			KthuraProp(int32, scalex);
+			KthuraProp(int32, scaley);
+			KthuraProp(int32, rotatedeg); KthuraPropAlias(int32, rotatedeg, rotate);
+			KthuraProp(double, rotaterad);
+			KthuraProp(int32, blend);
+			void data(std::string,std::string);
+			std::string data(std::string);
+			std::map<std::string, std::string>* data();
+
 		};
 
 		class KthuraLayer {
@@ -91,17 +138,21 @@ namespace Slyvina {
 			KthuraObject* _lastObject;
 			bool _modified{ false };
 			bool _autoRemap{ true };
-			KthuraObject* NewObject(KthuraKind k);
 			std::map<uint64, KthuraObject*> IDMap{};
 			std::map<std::string, KthuraObject*> TagMap{};
-			std::map <std::string, vector<KthuraObject*>> _LabelMap;
+			std::map<std::string, std::vector<KthuraObject*>> _LabelMap;
 		public:
 			KthuraObject* DomFirst{ nullptr };
+			uint32
+				gridx{ 32 },
+				gridy{ 32 };
+			KthuraObject* NewObject(KthuraKind k);
 			void PerformAutoRemap();
 			inline void __setparent(_Kthura* ouwe) { if (!_parent) _parent = ouwe; } // Only works when not yet initized. Not for direct use			
 			void Kill(KthuraObject* obj);
 			void Kill(std::string Tag,bool ignorenonexistent=true);
 			void KillAllObjects();
+			uint64 CountObjects();
 			void TotalRemap();
 			inline bool HasID(uint64 i) { return IDMap.count(i); }
 			inline bool HasTag(std::string T) { Units::Trans2Upper(T); return TagMap.count(T); }
@@ -109,11 +160,22 @@ namespace Slyvina {
 			void RemapTags();
 			void RemapLabels();
 			void RemapDominance();
+			inline KthuraObject* FirstObject() { return _firstObject; }
 			KthuraObject* Obj(uint64 i);
 			KthuraObject* NewTiledArea(int32 x = 0, int32 y = 0, int32 w = 0, int32 h = 0,std::string Texture="",std::string Tag="");
+			KthuraObject* NewObstacle(int32 x = 0, int32 y = 0, std::string Texture = "", std::string Tag = "");
 			inline ~KthuraLayer() { KillAllObjects(); }
 			inline void AutoRemap(bool onoff) { _autoRemap = onoff; PerformAutoRemap(); }
-			inline bool AutoRemap() { return _autoRemap; }
+			inline bool AutoRemap() { return _autoRemap; }			
 		};
+
+
+
+		inline Kthura LoadKthura(Slyvina::JCR6::JT_Dir J, std::string prefix="") { return std::make_shared<_Kthura>(J, prefix); }
+		Kthura LoadKthura(std::string resfile, std::string prefix = "");
+		inline UKthura LoadUKthura(Slyvina::JCR6::JT_Dir J, std::string prefix = "") { return std::make_unique<_Kthura>(J, prefix); }
+		UKthura LoadUKthura(std::string resfile,  std::string prefix = "");
+		inline Kthura CreateKthura() { return std::make_shared<_Kthura>(); }
+		inline UKthura CreateUniqueKthura() { return std::make_unique<_Kthura>(); }
 	}
 }
--- Source/Kthura_Core.cpp
+++ Source/Kthura_Core.cpp
@@ -1,12 +1,43 @@
+// Lic:
+// Kthura/Source/Kthura_Core.cpp
+// Slyvina - Kthura Core
+// version: 22.12.15
+// Copyright (C) 2022 Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
 #include <iostream>
 #include <SlyvString.hpp>
+#include <SlyvSTOI.hpp>
 #include <Kthura_Core.hpp>
+
+#define KthuraCoreDebug
+
+
 
 #define KthuraObjVal(type,prop) void KthuraObject::prop(type v) {_Obj->prop=v;} type KthuraObject::prop() {return _Obj->prop;}
 #define KthuraObjValRP(type,prop) void KthuraObject::prop(type v) {_Obj->prop=v; _parent->PerformAutoRemap();} type KthuraObject::prop() {return _Obj->prop;}
+#define KthuraObjValDefFunc(type,prop)  type KthuraObject::prop() { return _Obj->prop;} void KthuraObject::prop(type value) 
 
+#ifdef KthuraCoreDebug
+#define Chat(msg) cout << "\x1b[33mKthura Debug>\t\x1b[0m" << msg << endl
+#else
+define Chat(msg)
+#endif
 using namespace std;
 using namespace Slyvina::Units;
+using namespace Slyvina::JCR6;
 
 namespace Slyvina {
 	namespace Kthura {
@@ -15,7 +46,7 @@ namespace Slyvina {
 		KthuraPanicFunction KthuraPanic{ nullptr };
 
 		static void DefPaniek(std::string msg, std::string xdata) {
-			cout << "\x1b[31mKTHURA ERROR!\x1b[0m;\t";
+			cout << "\x1b[31mKTHURA ERROR!\x1b[0m\t"<<msg<<endl;
 			auto xd{ Split(xdata,';') };
 			for (auto d : *xd) {
 				auto s{ Split(d,':') };
@@ -32,13 +63,17 @@ namespace Slyvina {
 			exit(100);
 		}
 
-		inline void Paniek(std::string msg, std::string xdata="Note:No further data provided") {
+		inline void Paniek(std::string msg, std::string xdata = "Note:No further data provided") {
 			if (!KthuraPanic) KthuraPanic = DefPaniek;
 			KthuraPanic(msg, xdata);
 		}
 
 #pragma endregion
 
+#pragma region EntireMap
+		uint64 _Kthura::_KthCount{0};
+		bool _Kthura::AllowUnknown{ false };
+#pragma endregion
 
 #pragma region Layers
 		void _Kthura::_LayerRemap() { _LayersMap.clear(); for (auto& l : _Layers) _LayersMap.push_back(l.first); }
@@ -78,11 +113,11 @@ namespace Slyvina {
 			delete k;
 			PerformAutoRemap();
 		}
-		void KthuraLayer::Kill(std::string Tag,bool ignorenonexistent) {
+		void KthuraLayer::Kill(std::string Tag, bool ignorenonexistent) {
 			Trans2Upper(Tag);
 			if (!TagMap.count(Tag)) {
 				if (ignorenonexistent) return;
-				Paniek("Trying to kill an object with a non-existent tag","Tag:"+Tag);
+				Paniek("Trying to kill an object with a non-existent tag", "Tag:" + Tag);
 				return;
 			}
 			Kill(TagMap[Tag]);
@@ -106,9 +141,17 @@ namespace Slyvina {
 		void KthuraLayer::RemapTags() {
 			TagMap.clear();
 			for (auto o = _firstObject; o; o = o->Next()) {
-				if (TagMap.count(o->Tag())) Paniek("RemapTags(): Dupe tag (" + o->Tag() + ")");
-				TagMap[o->Tag()] = o;
+				if (o->Tag().size()) {
+					if (TagMap.count(o->Tag())) Paniek("RemapTags(): Dupe tag (" + o->Tag() + ")");
+					TagMap[o->Tag()] = o;
+				}
 			}
+		}
+
+		uint64 KthuraLayer::CountObjects() {
+			uint64 ret{ 0 };
+			for (auto o = _firstObject; o; o = o->Next()) ret++;
+			return ret;
 		}
 
 		void KthuraLayer::RemapLabels() {
@@ -131,7 +174,7 @@ namespace Slyvina {
 			}
 			KthuraObject* Last{ nullptr };
 			for (auto& dom : tempmap) {
-				if (!Last) 
+				if (!Last)
 					DomFirst = dom.second;
 				else {
 					Last->DomNext = dom.second;
@@ -151,6 +194,16 @@ namespace Slyvina {
 			return o;
 		}
 
+		KthuraObject* KthuraLayer::NewObstacle(int32 x, int32 y, std::string Texture, std::string Tag) {
+			auto o = NewObject(KthuraKind::Obstacle);
+			o->x(x);
+			o->y(y);
+			o->texture(Texture);
+			o->Tag(Tag);
+			return o;
+
+		}
+
 		void KthuraLayer::TotalRemap() {
 			RemapTags();
 #pragma message ("Still gotta do remapping by dominance")
@@ -162,6 +215,8 @@ namespace Slyvina {
 		KthuraObject* KthuraLayer::NewObject(KthuraKind k) {
 			while (IDMap.count(++_objcnt)) {}
 			auto o = new KthuraObject(_lastObject, this, k, _objcnt);
+			if (!_firstObject) _firstObject = o;
+			_lastObject = o;
 			_modified = true;
 			return o;
 		}
@@ -172,7 +227,7 @@ namespace Slyvina {
 		}
 #pragma endregion
 
-#pragma Objects
+#pragma region Objects
 		struct __KthuraObjectData {
 			KthuraObject* parent{ nullptr };
 			int32
@@ -180,15 +235,25 @@ namespace Slyvina {
 				y{ 0 },
 				w{ 0 },
 				h{ 0 },
-				animspeed{-1};
+				animspeed{ -1 },
+				insertx{ 0 },
+				inserty{ 0 },
+				scalex{ 1000 },
+				scaley{ 1000 },
+				rotatedeg{ 0 };
+			double
+				truescalex{ 1 },
+				truescaley{ 1 },
+				rotaterad{ 0 };
 			uint32
 				dominance{ 20 },
 				animskip{ 0 },
-				animframe{ 0 };
-			std::string 
-				Tag{""},
-				texture{""},
-				labels{""};
+				animframe{ 0 },
+				blend{ 0 };
+			std::string
+				Tag{ "" },
+				texture{ "" },
+				labels{ "" };
 			byte
 				r{ 255 },
 				g{ 255 },
@@ -198,13 +263,15 @@ namespace Slyvina {
 				impassible{ false },
 				forcepassible{ false },
 				visible{ true };
+			map<string,string>
+				data{ };
 		};
 
 		struct __KthuraActorData {
 			KthuraObject* parent{ nullptr };
 		};
 
-		KthuraObject::KthuraObject(KthuraObject* _after,KthuraLayer* _ouwe,KthuraKind k,uint64 _giveID) {
+		KthuraObject::KthuraObject(KthuraObject* _after, KthuraLayer* _ouwe, KthuraKind k, uint64 _giveID) {
 			_parent = _ouwe;
 			if (_after) {
 				_next = _after->_next;
@@ -232,11 +299,15 @@ namespace Slyvina {
 			_parent->PerformAutoRemap();
 		}
 		std::string KthuraObject::Tag() { return _Obj->Tag; }
-		
+
 		KthuraObjVal(int32, x);
 		KthuraObjVal(int32, y);
 		KthuraObjVal(int32, w);
 		KthuraObjVal(int32, h);
+		KthuraObjVal(byte, r);
+		KthuraObjVal(byte, g);
+		KthuraObjVal(byte, b);
+		KthuraObjVal(byte, a);
 		KthuraObjVal(bool, visible);
 		KthuraObjValRP(bool, impassible);
 		KthuraObjValRP(bool, forcepassible);
@@ -245,5 +316,244 @@ namespace Slyvina {
 		KthuraObjValRP(uint32, dominance);
 		KthuraObjVal(uint32, animframe);
 		KthuraObjVal(int32, animspeed);
+		KthuraObjVal(int32, insertx);
+		KthuraObjVal(int32, inserty);
+		KthuraObjValDefFunc(int32, scalex) { _Obj->scalex = value; _Obj->truescalex = (double)value / (double)1000; }
+		KthuraObjValDefFunc(int32, scaley) { _Obj->scaley = value; _Obj->truescaley = (double)value / (double)1000; }
+		KthuraObjValDefFunc(int, rotatedeg) { _Obj->rotatedeg = value; _Obj->rotaterad = (double)(((double)value) * (180.0 / PI)); }
+		KthuraObjValDefFunc(double, rotaterad) { _Obj->rotaterad = value; _Obj->rotatedeg = (int)floor(value * (180.0 / PI)); }
+		KthuraObjVal(int32, blend);
+		void KthuraObject::data(std::string k, std::string v) { _Obj->data[Upper(k)] = v; }
+		std::string KthuraObject::data(std::string k) { return _Obj->data[Upper(k)]; }
+		std::map<std::string, std::string>* KthuraObject::data() { return &(_Obj->data); }
+
+
+		static map<KthuraKind, string> _KindName{
+			{KthuraKind::Unknown,"Unknown"},
+			{KthuraKind::Actor,"Actor"},
+			{KthuraKind::Obstacle,"Obstacle"},
+			{KthuraKind::Picture,"Picture"},
+			{KthuraKind::Rect,"Rect"},
+			{KthuraKind::StretchedArea,"StretchedArea"},
+			{KthuraKind::TiledArea,"TiledArea"},
+			{KthuraKind::Zone,"Zone"},
+			{KthuraKind::Custom,"Custom"},
+			{KthuraKind::Pivot,"Pivor"},
+			{KthuraKind::Exit,"Exit"}
+		};
+
+		std::string KindName(KthuraKind k) {
+			Chat("Kind requestsed (" << (int)k << ")   Found: " << boolstring(_KindName.count(k)));
+			if (_KindName.count(k)) {
+				Chat("Returning: " << _KindName[k]);
+				return _KindName[k];
+			}
+			return "Unknown";
+		}
+
+		KthuraKind KindName(std::string s) {
+			if (!s.size()) return KthuraKind::Unknown;
+			if (s[0] == '$') return KthuraKind::Custom;
+			for (auto& kn : _KindName) {
+				if (Upper(s) == Upper(kn.second)) return kn.first;
+			}
+			return KthuraKind::Unknown;
+		}
+		std::string KthuraObject::SKind() { 
+			if (_Kind == KthuraKind::Custom) return CustomKind; else return KindName(_Kind); 
+		}
+#pragma endregion
+
+#pragma region Loader
+#define I_Want_An_Object if (!co) { Paniek("I cannot define field '"+fld+"' without an object",TrSPrintF("Line:%d;Instruction:%s",line,instruction.c_str())); return; }
+#define SyntaxError { Paniek("Syntax error", TrSPrintF("Line:%d;Instruction:%s", line, instruction.c_str())); return; }
+
+		_Kthura::_Kthura(Slyvina::JCR6::JT_Dir Resource, std::string prefix) {
+			if (prefix.size() && (!Suffixed(prefix, "/"))) prefix += "/";
+			if (!Resource->EntryExists(prefix + "Data")) { Paniek("JCR6 resource doesn't appear to have the '" + prefix + "Data' entry that Kthura needs"); return; }
+			if (!Resource->EntryExists(prefix + "Objects")) { Paniek("JCR6 resource doesn't appear to have the '" + prefix + "Objects' entry that Kthura needs"); return; }
+			auto entries{ Resource->Entries() };
+			for (auto ent : *entries) {
+				auto n = ent->Name().substr(prefix.size());
+				auto nu = Upper(n);
+				Chat("Processing entry: " << n);
+				if (nu == "DATA") {
+					Chat("Reading Data");
+					MetaData = Resource->GetStringMap(prefix + "Data");
+					if ((!MetaData) || Last()->Error) {
+						Paniek("Something went wrong reading data", "JCR6:" + Last()->ErrorMessage); return;
+					}
+				} else if (nu == "OBJECTS") {
+					Chat("Reading objects");
+					auto Source{ Resource->GetLines(prefix + "Objects") };
+					auto LayerScope{ false };
+					KthuraObject* co{ nullptr };
+					KthuraLayer* cl{ nullptr };
+					for (size_t _line = 0; _line < Source->size(); _line++) {
+						auto line{ _line + 1 }; // Chat("Parsing line: " << line << " (Vector position: " << _line << ") out of " << Source->size());
+						auto instruction{ Trim((*Source)[_line]) };
+						if (instruction == "" || Prefixed(instruction, "--")) {
+							// Do nothing at all. These lines have no values!
+						} else if (LayerScope) {
+							if (instruction == "__END") {
+								Chat("End Layer Scope");
+								LayerScope = false;
+							} else {
+								Chat("Creating new layer: " << instruction);
+								NewLayer(instruction)->AutoRemap(false);
+							}
+							Chat("Layer scope " << boolstring(LayerScope) << "; '" << instruction << "'" << "; HMMMM: " << boolstring(instruction == "__END"));
+						} else if (instruction == "LAYERS") {
+							LayerScope = true;
+						} else if (instruction == "NEW") {
+							if (!cl) { Paniek("Object without a layer", TrSPrintF("Line:%d", line)); return; }
+							co = cl->NewObject(KthuraKind::Unknown);
+							Chat("Objects in layer: " << cl->CountObjects());
+						} else {
+							auto isteken{ FindFirst(instruction,'=') };
+							if (isteken < 0) SyntaxError;
+							auto fld = Trim(instruction.substr(0, isteken));
+							auto val = Trim(instruction.substr(isteken + 1));
+							if (fld == "KIND") {
+								I_Want_An_Object;
+								co->IKind(KindName(val));
+								if (co->Kind() == KthuraKind::Unknown) { Paniek("Unknown kind", TrSPrintF("Line:%d;Instruction:%s", line, instruction.c_str())); }
+								if (co->Kind() == KthuraKind::Actor) { Paniek("Actors cannot be defined through loading", TrSPrintF("Line:%d;Instruction:%s", line, instruction.c_str())); }
+								if (co->Kind() == KthuraKind::Custom) co->CustomKind = val;
+							} else if (fld == "COORD") {
+								I_Want_An_Object;
+								auto cs = Split(val, ',');
+								if (cs->size() < 2) SyntaxError;
+								int32
+									x{ ToInt((*cs)[0]) },
+									y{ ToInt((*cs)[1]) };
+								co->x(x);
+								co->y(y);
+
+							} else if (fld == "INSERT") {
+								I_Want_An_Object;
+								auto cs = Split(val, ',');
+								if (cs->size() < 2) SyntaxError;
+								int32
+									x{ ToInt((*cs)[0]) },
+									y{ ToInt((*cs)[1]) };
+								co->insertx(x);
+								co->inserty(y);
+							} else if (fld == "ROTATION") {
+								I_Want_An_Object;
+								co->rotatedeg(ToInt(val)); // Please note! Rotations are only stored in degrees. That was first of all the BlitzMax standard what Kthura was originally in, but radians also force me to work in decimals and that's a recipe for disaster.
+							} else if (fld == "SIZE") {
+								I_Want_An_Object;
+								auto cs = Split(val, 'x');
+								if (cs->size() < 2) SyntaxError;
+								int32
+									w{ ToInt((*cs)[0]) },
+									h{ ToInt((*cs)[1]) };
+								co->w(w);
+								co->h(h);
+							} else if (fld == "TAG") {
+								I_Want_An_Object;
+								co->Tag(val);
+							} else if (fld == "LABELS") {
+								I_Want_An_Object;
+								co->labels(val);
+							} else if (fld == "DOMINANCE") {
+								I_Want_An_Object;
+								co->dominance(ToInt(val));
+							} else if (fld == "TEXTURE") {
+								I_Want_An_Object;
+								co->texture(val);
+							} else if (fld == "ANIMSPEED" || fld == "FRAMESPEED") {
+								I_Want_An_Object;
+								co->animspeed(ToInt(val));
+							} else if (fld == "ANIMFRAME" || fld == "CURRENTFRAME") {
+								I_Want_An_Object;
+								co->animframe(ToInt(val));
+							} else if (fld == "ALPHA") {
+								Paniek("The ALPHA field was deprecated ages ago and has by now officially been removed", TrSPrintF("Line:%d\nNote ALPHA255 should be used in stead!", line));
+								return;
+							} else if (fld == "ALPHA255") {
+								I_Want_An_Object;
+								co->alpha((byte)ToInt(val));
+							} else if (fld == "VISIBLE") {
+								I_Want_An_Object;
+								co->visible(ToInt(val) > 0);
+							} else if (fld == "COLOR") {
+								I_Want_An_Object;
+								auto cs = Split(val, ',');
+								if (cs->size() < 3) SyntaxError;
+								byte
+									r{ (byte)ToInt((*cs)[0]) },
+									g{ (byte)ToInt((*cs)[1]) },
+									b{ (byte)ToInt((*cs)[2]) };
+								co->red(r);
+								co->green(g);
+								co->blue(b);
+							} else if (fld == "IMPASSIBLE") {
+								I_Want_An_Object;
+								co->impassible(ToInt(val) > 0);
+							} else if (fld == "FORCEPASSIBLE") {
+								I_Want_An_Object;
+								co->forcepassible(ToInt(val) > 0);
+							} else if (fld == "SCALE") {
+								I_Want_An_Object;
+								auto cs = Split(val, ',');
+								if (cs->size() < 2) SyntaxError;
+								int32
+									x{ ToInt((*cs)[0]) },
+									y{ ToInt((*cs)[1]) };
+								co->scalex(x);
+								co->scaley(y);
+							} else if (fld == "BLEND") {
+								I_Want_An_Object;
+								co->blend(ToInt(val));
+							} else if (Prefixed(fld, "DATA.")) {
+								I_Want_An_Object;
+								auto dfld = fld.substr(5);
+								Chat("Defining data field: " << dfld << " = " << val);
+								co->data(dfld, val);
+							} else if (fld == "LAYER") {
+								if (cl) cl->AutoRemap(true);
+								if (!HasLayer(val)) { Paniek("Trying to switch to non-existent layer", TrSPrintF("Line:%d;Layer:%s", line, val.c_str())); return; }
+								cl = Layer(val);
+							} else if (fld == "BLOCKMAPGRID") {
+								if (!cl) { Paniek("Grid definition without a layer", TrSPrintF("Line:%d", line)); return; }
+								auto cs = Split(val, 'x');
+								if (cs->size() < 2) SyntaxError;
+								int32
+									x{ ToInt((*cs)[0]) },
+									y{ ToInt((*cs)[1]) };
+								cl->gridx = x;
+								cl->gridy = y;
+
+							} else { Paniek("Unknown field!", TrSPrintF("Line:%d;Instruction:%s", line, instruction.c_str())); }
+						}
+					}
+					if (cl) cl->AutoRemap(true);
+				} else if (nu == "OPTIONS") {
+					cout << "OPTIONS NOT YET IMPLEMENTED! DATA WILL BE LOST!\n";
+				} else if (AllowUnknown) {
+					Chat("Loaded Uknown: " << n);
+					Unknown[n] = Resource->B(ent->Name());
+				}
+			}
+		}
+
+		Kthura LoadKthura(std::string resfile, std::string prefix) {
+			auto J{ JCR6_Dir(resfile) };
+			if (Last()->Error) {
+				Paniek("JCR6 error", "Kthura Map file:" + resfile + ";Prefix:" + prefix); return nullptr;
+			}
+			return LoadKthura(J, prefix);
+		}
+
+		UKthura LoadUKthura(std::string resfile, std::string prefix) {
+			auto J{ JCR6_Dir(resfile) };
+			if (Last()->Error) {
+				Paniek("JCR6 error", "Kthura Map file:" + resfile + ";Prefix:" + prefix); return nullptr;
+			}
+			return std::make_unique<_Kthura>(J, prefix);
+		}
+#pragma endregion
 	}
 }
