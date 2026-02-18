1a7828c2f58dfa7aae8c4203a7ef4b241fcedaec
Jeroen P. Broks <facebook@tbbs.nl>
2026-02-18 12:57:30 +0100

    Update Feb 2026

--- Source/Kthura_Draw_TQSG.cpp
+++ Source/Kthura_Draw_TQSG.cpp
@@ -1,9 +1,9 @@
 // License:
 // 	Kthura/Source/Kthura_Draw_TQSG.cpp
 // 	Kthura Draw TQSG driver
-// 	version: 25.03.01
+// 	version: 26.02.18
 // 
-// 	Copyright (C) 2023, 2025 Jeroen P. Broks
+// 	Copyright (C) 2023, 2025, 2026 Jeroen P. Broks
 // 
 // 	This software is provided 'as-is', without any express or implied
 // 	warranty.  In no event will the authors be held liable for any damages
@@ -21,25 +21,6 @@
 // 	   misrepresented as being the original software.
 // 	3. This notice may not be removed or altered from any source distribution.
 // End License
-// Lic:
-// Kthura/Source/Kthura_Draw_TQSG.cpp
-// Kthura Draw TQSG driver
-// version: 23.11.21
-// Copyright (C) 2023 Jeroen P. Broks
-// This software is provided 'as-is', without any express or implied
-// warranty.  In no event will the authors be held liable for any damages
-// arising from the use of this software.
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-// 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software
-// in a product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-// 2. Altered source versions must be plainly marked as such, and must not be
-// misrepresented as being the original software.
-// 3. This notice may not be removed or altered from any source distribution.
-// EndLic
 
 
 #undef KD_TQSG_DEBUG
@@ -57,6 +38,8 @@
 #define Chat(m)
 #define DCOUT(m)
 #endif
+
+#include <algorithm>
 
 #include "../Headers/Kthura_Draw_TQSG.hpp"
 
@@ -312,8 +295,8 @@ namespace Slyvina {
 				return	{
 					(int)(o->x()-((Tex->Width()/2)*((double)o->scalex()/1000))),
 					(int)(o->y()-(Tex->Height()*((double)o->scaley()/1000))),
-					(int)(Tex->Width()*((double)o->scalex()/1000)),
-					(int)(Tex->Height() * ((double)o->scaley() / 1000))
+					(int)(Tex->Width()*std::abs((double)o->scalex()/1000)),
+					(int)(Tex->Height()*std::abs((double)o->scaley() / 1000))
 				};
 			case KthuraKind::Pivot:
 			case KthuraKind::Exit:
